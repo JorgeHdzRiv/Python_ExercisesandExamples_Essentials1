bf0e408f64839bc0f078db6c4da3389e255bcc45
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-28 01:09:33 -0500

    Lab2: print con end,sep

---
+++ Exercises/Lab2_print2.py
@@ -0,0 +1,18 @@
+"""
+Escenario
+Modifica la primera línea de código en el editor, utilizando las palabras clave sep y end, para que coincida con el resultado esperado. Recuerda, utilizar dos funciones print().
+
+No cambies nada en la segunda invocación de print().
+
+Salida Esperada
+Fundamentos***Programación***en...Python
+
+"""
+
+print("Fundamentos","Programación","en")
+print("Python")
+
+#Solucion
+print("Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
+
