7130b2e20d75f28c9f57f4e127e5bc8dfbd7bb0b
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-28 01:03:01 -0500

    Agregando mas lineas a primer programa

--- Examples/primer_programa.py
+++ Examples/primer_programa.py
@@ -1,14 +1,27 @@
+#Esto es un comentario
 
+#print simboliza una funcion que esta integrada en el lenguaje
 print("Hola mundo")
+print() #Esto genera una linea vacia util para saltos de linea
 print("Introduccion a Python")
+print()
+print("Salto de linea \nen el mismo print") #\n Genera un salto de linea antes o despues
+
+print("Mi nombre es", "Jorge") #Pasando varios argumentos
+print("Tengo",26)              #La coma simboliza un espacio despues del argumento
+
+print("Argumento con","palabra clave", end=" ") 
+print("Monty Python.")
+#end determina los caracteres que la función print() envía a la salida una vez que llega al final de sus argumentos posicionales.
+
+print("Mi", "nombre", "es", "Jorge", "Hdz.", sep="-")
+#sep asignara un separador despues de cada argumento , se puede pasar vacio y sera un espacio
 
 #Esto es un comentario
 
 """
 Esto es un comentario multilinea
 """
-
-#print simboliza una funcion que esta integrada en el lenguaje
 
 
 """
