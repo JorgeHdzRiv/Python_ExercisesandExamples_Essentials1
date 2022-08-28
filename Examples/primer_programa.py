1cb4f6c3b7fd25e440c7b0a173798303317643b3
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-28 00:42:57 -0500

    Primer_programa

---
+++ Examples/primer_programa.py
@@ -0,0 +1,20 @@
+
+print("Hola mundo")
+print("Introduccion a Python")
+
+#Esto es un comentario
+
+"""
+Esto es un comentario multilinea
+"""
+
+#print simboliza una funcion que esta integrada en el lenguaje
+
+
+"""
+Como se dijo anteriormente, una función puede tener:
+
+Un efecto.
+Un resultado.
+"""
+
