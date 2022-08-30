66573611e29af247243bd88749262fb97fb84848
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-30 12:38:24 -0500

    Lab4:Literales

---
+++ Exercises/Lab4_Literales.py
@@ -0,0 +1,19 @@
+"""
+Objetivos
+Familiarizarse con la función print() y sus capacidades de formato.
+Practicar el codificar cadenas.
+Experimentar con el código de Python.
+
+Escenario
+Escribe una sola línea de código, utilizando la función print(), así como los caracteres de nueva línea y escape, para obtener la salida esperada de tres líneas.
+
+"""
+
+# Salida Esperada
+
+# "Estoy"
+# ""aprendiendo""
+# """Python"""
+
+print("Solucion")
+print('"Estoy"\n""aprendiendo""\n"""Python"""')
