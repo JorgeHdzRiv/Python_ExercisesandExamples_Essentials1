a69d5c1cb2d9ea61e9340f8561d75296693e175a
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-30 15:02:02 -0500

    Lab5:Variables

---
+++ Exercises/Lab5_Variables.py
@@ -0,0 +1,33 @@
+"""
+Objetivos
+Familiarizarse con el concepto de almacenar y trabajar con diferentes tipos de datos en Python.
+Experimentar con el código en Python.
+
+Escenario
+A continuación una historia:
+
+Érase una vez en la Tierra de las Manzanas, Juan tenía tres manzanas, María tenía cinco manzanas, y Adán tenía seis manzanas. 
+Todos eran muy felices y vivieron por muchísimo tiempo. Fin de la Historia.
+
+Tu tarea es:
+
+*Crear las variables: juan, maria, y adan.
+*Asignar valores a las variables. El valor debe de ser igual al número de manzanas que cada quien tenía.
+*Una vez almacenados los números en las variables, imprimir las variables en una línea, y separar cada una de ellas con una coma.
+*Después se debe crear una nueva variable llamada total_manzanas y se debe igualar a la suma de las tres variables anteriores.
+*Imprime el valor almacenado en total_manzanas en la consola.
+*Experimenta con tu código: crea nuevas variables, asigna diferentes valores a ellas, 
+y realiza varias operaciones aritméticas con ellas (por ejemplo, +, -, *, /, //, etc.).
+Intenta poner una cadena con un entero juntos en la misma línea, por ejemplo, "Número Total de Manzanas:" y total_manzanas.
+
+"""
+#Variables en una sola linea
+juan = 3; maria =5; adan=6
+#juan,maria,adan = 3,5,6
+
+print("Juan: ",juan,"\nMaria: ",maria,"\nAdan: ",adan)
+
+total_manzanas = juan+maria+adan
+
+print("El total de manzanas es: ",total_manzanas)
+
