df8235a6a6e9548cea5a6a27a9ece2996a6c63a5
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-31 15:29:30 -0500

    Operadores comparacion

---
+++ Examples/OperadoresComparacion.py
@@ -0,0 +1,29 @@
+#Existen dos valores booleanos en Python True and False
+
+print(2 == 2)
+print(2 == 0)
+
+#Mayor que y menor que
+print(2 > 0)
+print(2 < 0)
+
+#Mayor igual que y menor igual que
+print(8 >= 5)
+print(5 <= 5)
+
+#Guardar resultado en variable
+res = (5>= 3)
+print(res)
+
+"""
+Ahora necesitamos actualizar nuestra tabla de prioridades, y poner todos los nuevos operadores en ella. Ahora se ve como a continuación:
+
+Prioridad	                Operador	
+1	        +, -	        unario
+2	        **	
+3	        *, /, //, %	
+4	        +, -	        binario
+5	        <, <=, >, >=	
+6	        ==, !=	
+
+"""
