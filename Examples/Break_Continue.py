5b27b48e5d290783507c053b5a71d81f3d38f79a
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-09-02 13:37:24 -0500

    Break and Continue

---
+++ Examples/Break_Continue.py
@@ -0,0 +1,73 @@
+"""
+break: sale del bucle inmediatamente, e incondicionalmente termina la operación del bucle; 
+el programa comienza a ejecutar la instrucción más cercana después del cuerpo del bucle.
+
+continue: se comporta como si el programa hubiera llegado repentinamente al final del cuerpo; 
+el siguiente turno se inicia y la expresión de condición se prueba de inmediato.
+
+Ambas palabras son palabras clave reservadas.
+"""
+# break - ejemplo
+
+print("La instrucción break:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Dentro del bucle.", i)
+print("Fuera del bucle.")
+
+
+# continue - ejemplo
+
+print("\nLa instrucción continue:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Dentro del bucle.", i)
+print("Fuera del bucle.")
+
+"""
+#Numero mas grande con break
+
+largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+
+if counter != 0:
+    print("El número más grande es", largest_number)
+else:
+    print("No has ingresado ningún número.")
+"""
+
+"""
+#Numero mas grande con continue
+
+largest_number = -99999999
+counter = 0
+
+number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+
+if counter:
+    print("El número más grande es", largest_number)
+else:
+    print("No has ingresado ningún número.")
+
+
+
+"""
