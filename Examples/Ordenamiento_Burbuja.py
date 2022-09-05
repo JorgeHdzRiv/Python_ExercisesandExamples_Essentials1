ba17a478c3e86178d2c0663ce608a4bcc91c0653
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-09-05 14:53:27 -0500

    Ordenamiento burbuja

---
+++ Examples/Ordenamiento_Burbuja.py
@@ -0,0 +1,33 @@
+"""
+my_list = [8, 10, 6, 2, 4]  # lista a ordenar
+swapped = True  # Lo necesitamos verdadero (True) para ingresar al bucle while.
+
+while swapped:
+    swapped = False  # no hay intercambios hasta ahora
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True  # ¡ocurrió el intercambio!
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
+
+"""
+
+#Ordenamiento burbuja mejorado
+my_list = []
+swapped = True
+num = int(input("¿Cuántos elementos deseas ordenar?: "))
+
+for i in range(num):
+    val = float(input("Ingresa un elemento de la lista: "))
+    my_list.append(val)
+
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print("\nOrdenada:")
+print(my_list)
