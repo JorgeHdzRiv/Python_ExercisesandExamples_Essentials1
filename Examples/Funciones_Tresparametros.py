413142a42a0115d23dfabe3764b1fd2bd3845cf9
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-09-07 16:47:55 -0500

    Funcion con tres parametros

---
+++ Examples/Funciones_Tresparametros.py
@@ -0,0 +1,45 @@
+def is_a_triangle(a, b, c):
+    if a + b <= c:
+        return False
+    if b + c <= a:
+        return False
+    if c + a <= b:
+        return False
+    return True
+
+
+print(is_a_triangle(1, 1, 1))
+print(is_a_triangle(1, 1, 3))
+
+# Con teorema de pitagoras c^2 = a^2 + b^2
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Ingresa la longitud del primer lado: '))
+b = float(input('Ingresa la longitud del segundo lado: '))
+c = float(input('Ingresa la longitud del tercer lado: '))
+
+if is_a_triangle(a, b, c):
+    print('Si, si puede ser un triángulo.')
+else:
+    print('No, no puede ser un triángulo.')
+
+# Area de un tringulo rectangulo A = sqrt(s(s-a)(s-b)(s-c)) donde s = (a+b+c)/2
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+
+
+print(area_of_triangle(1., 1., 2. ** .5))
+
