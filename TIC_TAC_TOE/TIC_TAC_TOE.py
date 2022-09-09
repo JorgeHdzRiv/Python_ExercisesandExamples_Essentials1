0b8b10b9f853abab0ea2325dccdf2300f7208b92
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-09-09 12:57:44 -0500

    Creacion del tablero

--- TIC_TAC_TOE/TIC_TAC_TOE.py
+++ TIC_TAC_TOE/TIC_TAC_TOE.py
@@ -1,9 +1,18 @@
+from random import randrange
+
 def DisplayBoard(board):
     # La función acepta un parámetro el cual contiene el estado actual del tablero
     # y lo muestra en la consola.
-    pass
     
-
+    print("+-------" * 3,"+", sep="")
+    for row in range(3):
+        print("|       " * 3,"|", sep="")
+        for col in range(3):
+            print("|   " + str(board[row][col]) + "   ", end="")
+        print("|")
+        print("|       " * 3,"|",sep="")
+        print("+-------" * 3,"+",sep="")
+            
 
 def EnterMove(board):
     # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,
@@ -23,3 +32,6 @@ def VictoryFor(board, sign):
 def DrawMove(board):
     # La función dibuja el movimiento de la máquina y actualiza el tablero.
     pass
+
+board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # crear un tablero
+DisplayBoard(board)
