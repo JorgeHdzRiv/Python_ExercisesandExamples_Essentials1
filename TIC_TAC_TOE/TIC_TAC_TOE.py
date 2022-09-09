1365007a8ec2f625afd2afd637d37ca9a6052f46
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-09-09 12:37:30 -0500

    Inicio del proyecto

---
+++ TIC_TAC_TOE/TIC_TAC_TOE.py
@@ -0,0 +1,25 @@
+def DisplayBoard(board):
+    # La función acepta un parámetro el cual contiene el estado actual del tablero
+    # y lo muestra en la consola.
+    pass
+    
+
+
+def EnterMove(board):
+    # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,
+    # verifica la entrada y actualiza el tablero acorde a la decisión del usuario.
+    pass
+
+def MakeListOfFreeFields(board):
+    # La función examina el tablero y construye una lista de todos los cuadros vacíos.
+    # La lista esta compuesta por tuplas, cada tupla es un par de números que indican la fila y columna.
+    pass
+
+def VictoryFor(board, sign):
+    # La función analiza el estatus del tablero para verificar si
+    # el jugador que utiliza las 'O's o las 'X's ha ganado el juego.
+    pass
+
+def DrawMove(board):
+    # La función dibuja el movimiento de la máquina y actualiza el tablero.
+    pass
